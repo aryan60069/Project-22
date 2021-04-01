b9a34a3ebc06b641ce5695c5da72be0a689af30a
aryan60069 <78200051+aryan60069@users.noreply.github.com>
2021-04-01 20:40:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,10 +9,10 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload(){
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
 }
 
 function setup() {
